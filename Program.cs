f7d5b1ac84a7311d609344fb277da62933a65dad
Alexandr <hwr96m@gmail.com>
2022-07-25 19:16:43 +0500

    add Kestrel

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 namespace DocMod {
     public class Program {
@@ -17,6 +18,7 @@ namespace DocMod {
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseKestrel();
                 });
     }
 }
